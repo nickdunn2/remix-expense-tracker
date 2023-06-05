1f9d701544591edb1a08eb38c012cdb37257eaa3
Nick Dunn <nick.dunn@flosports.tv>
2023-06-05 09:53:30 -0500

    feat(expenses): add updateExpense() functionality

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -45,3 +45,19 @@ export async function addExpense(expenseData) {
     throw error
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    return await prisma.expense.update({
+      where: { id },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date)
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,6 +1,9 @@
 import ExpenseForm from '~/components/expenses/ExpenseForm'
 import Modal from '~/components/util/Modal'
 import { useNavigate } from '@remix-run/react'
+import { updateExpense } from '~/data/expenses.server'
+import { redirect } from '@remix-run/node'
+import { validateExpenseInput } from '~/data/validation.server'
 
 export default function UpdateExpensesPage() {
   const navigate = useNavigate()
@@ -14,4 +17,19 @@ export default function UpdateExpensesPage() {
       <ExpenseForm />
     </Modal>
   )
+}
+
+export async function action({params, request}) {
+  const expenseId = params.id
+  const formData = await request.formData()
+  const expenseData = Object.fromEntries(formData)
+
+  try {
+    validateExpenseInput(expenseData)
+  } catch (err) {
+    return err
+  }
+
+  await updateExpense(expenseId, expenseData)
+  return redirect('/expenses')
 }
