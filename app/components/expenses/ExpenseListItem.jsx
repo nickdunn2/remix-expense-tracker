0d916386c5fc61aad5e1365c71cad553cd46a879
Nick Dunn <nick.dunn@flosports.tv>
2023-06-12 08:48:47 -0500

    WIP: useFetcher but it 404s

--- app/components/expenses/ExpenseListItem.jsx
+++ app/components/expenses/ExpenseListItem.jsx
@@ -1,6 +1,24 @@
-import { Form, Link } from '@remix-run/react'
+import { Link, useFetcher } from '@remix-run/react'
 
 function ExpenseListItem({ id, title, amount }) {
+  const fetcher = useFetcher()
+
+  function deleteExpenseItem() {
+    const shouldProceed = confirm('Are you sure you want to delete this item?')
+    if (!shouldProceed) return
+
+    fetcher.submit(null, { method: 'delete', action: `/expense/${id}` })
+  }
+
+  if (fetcher.state !== 'idle') {
+
+    return (
+      <article className="expense-item locked">
+        <p>Deleting...</p>
+      </article>
+    )
+  }
+
   return (
     <article className="expense-item">
       <div>
@@ -8,9 +26,7 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        <Form method="delete" action={`/expenses/${id}`}>
-          <button>Delete</button>
-        </Form>
+        <button onClick={deleteExpenseItem}>Delete</button>
         <Link to={id}>Edit</Link>
       </menu>
     </article>
