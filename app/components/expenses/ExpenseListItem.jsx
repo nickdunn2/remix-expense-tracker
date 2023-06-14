83be5e0aeabb0c7a71e526e8befa1228c728614d
Nick Dunn <nick.dunn@flosports.tv>
2023-06-14 10:00:24 -0500

    fix: revert broken useFetcher, with comment

--- app/components/expenses/ExpenseListItem.jsx
+++ app/components/expenses/ExpenseListItem.jsx
@@ -1,23 +1,28 @@
-import { Link, useFetcher } from '@remix-run/react'
+import { Form, Link, useFetcher } from '@remix-run/react'
 
 function ExpenseListItem({ id, title, amount }) {
-  const fetcher = useFetcher()
+  /**
+   * TODO
+   * Fetcher is NOT working for DELETE requests. It 404s and throws this error...
+   * "Error: You are calling submit during the server render. Try calling submit within a `useEffect` or callback instead."
+   */
+  // const fetcher = useFetcher()
 
-  function deleteExpenseItem() {
-    const shouldProceed = confirm('Are you sure you want to delete this item?')
-    if (!shouldProceed) return
-
-    fetcher.submit(null, { method: 'delete', action: `/expense/${id}` })
-  }
-
-  if (fetcher.state !== 'idle') {
-
-    return (
-      <article className="expense-item locked">
-        <p>Deleting...</p>
-      </article>
-    )
-  }
+  // function deleteExpenseItem() {
+  //   const shouldProceed = confirm('Are you sure you want to delete this item?')
+  //   if (!shouldProceed) return
+  //
+  //   fetcher.submit(null, { method: 'delete', action: `/expense/${id}` })
+  // }
+  //
+  // if (fetcher.state !== 'idle') {
+  //
+  //   return (
+  //     <article className="expense-item locked">
+  //       <p>Deleting...</p>
+  //     </article>
+  //   )
+  // }
 
   return (
     <article className="expense-item">
@@ -26,7 +31,10 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        <button onClick={deleteExpenseItem}>Delete</button>
+        {/*<button onClick={deleteExpenseItem}>Delete</button>*/}
+        <Form method="delete" action={`/expenses/${id}`}>
+          <button>Delete</button>
+        </Form>
         <Link to={id}>Edit</Link>
       </menu>
     </article>
