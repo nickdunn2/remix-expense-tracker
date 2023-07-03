fa0263b1dc81aecfd07f9378d67529306c089638
Nick Dunn <nick.dunn@flosports.tv>
2023-07-03 11:21:05 -0500

    feat(analysis): fetch expenses and display in chart

--- app/routes/__app/expenses.analysis.jsx
+++ app/routes/__app/expenses.analysis.jsx
@@ -1,11 +1,44 @@
 import Chart from '~/components/expenses/Chart'
 import ExpenseStatistics from '~/components/expenses/ExpenseStatistics'
+import { getExpenses } from '~/data/expenses.server'
+import { useCatch, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
+import Error from '~/components/util/Error'
 
 export default function ExpensesAnalysisPage() {
+  const expenses = useLoaderData()
+
   return (
     <main>
-      {/*<Chart expenses={} />*/}
-      {/*<ExpenseStatistics expenses={} />*/}
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
+    </main>
+  )
+}
+
+export async function loader() {
+  const expenses = await getExpenses()
+
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: 'Could not load expenses for analysis.'},
+      {
+        status: 404,
+        statusText: 'Expenses not found.'
+      }
+    )
+  }
+
+  return expenses
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch()
+  return (
+    <main>
+      <Error title={caughtResponse.statusText}>
+        <p>{caughtResponse.data?.message ?? 'Something went wrong. Could not load expenses.'}</p>
+      </Error>
     </main>
   )
 }
